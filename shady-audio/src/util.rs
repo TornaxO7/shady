2d8bcb668fb0e68b9bddc1a0456133d03086b985
TornaxO7 <tornax@proton.me>
2025-05-26 10:36:00 +0200

    docs(shady-audio, util): add some docs

--- shady-audio/src/util.rs
+++ shady-audio/src/util.rs
@@ -2,6 +2,7 @@ use cpal::traits::{DeviceTrait, HostTrait};
 
 type Devices = std::iter::Filter<cpal::Devices, for<'a> fn(&'a cpal::Device) -> bool>;
 
+/// A little helper enum to set the type of a device.
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Hash)]
 pub enum DeviceType {
     /// Input audio devices
@@ -12,6 +13,9 @@ pub enum DeviceType {
 
 /// Returns the given output/input device with the given name.
 /// You can retrieve a list of available names by using the [`get_device_names`] function.
+///
+/// Returns `Err` if there's a problem retrieving an output/input device.
+/// Returns `Ok(None)` if retrieveing the output/input devices worked find but it couldn't find a device with the given name.
 pub fn get_device<S: AsRef<str>>(
     name: S,
     device_type: DeviceType,
@@ -25,7 +29,7 @@ pub fn get_device<S: AsRef<str>>(
     }))
 }
 
-/// Returns the default device of he given device type.
+/// Returns the default device of he given device type (if available).
 pub fn get_default_device(device_type: DeviceType) -> Option<cpal::Device> {
     let host = cpal::default_host();
 
@@ -45,6 +49,7 @@ fn get_devices(device_type: DeviceType) -> Result<Devices, cpal::DevicesError> {
 }
 
 /// Returns a list of device names which you can use for [`get_device`].
+/// Retunrs `Err` if there's a problem retrieving an output/input device.
 pub fn get_device_names(device_type: DeviceType) -> Result<Vec<String>, cpal::DevicesError> {
     let devices = get_devices(device_type)?;
 
